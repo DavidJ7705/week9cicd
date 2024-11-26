0f5fa1d44bd4c55390859b939c62aafda3fbce54
G00419108@atu.ie <G00419108@atu.ie>
2024-11-26 11:37:49 +0000

    Successfully added a delete feature

--- src/main/java/ie/atu/week11example/PersonService.java
+++ src/main/java/ie/atu/week11example/PersonService.java
@@ -19,6 +19,11 @@ public class PersonService {
     public Person getPersonByEmployeeId(String employeeId) {
         // fetch data from a database in future lab
         // For simplicity, we return a dummy person here
-        return new Person();
+        return personRepository.findByEmployeeId(employeeId);
+
+    }
+
+    public void deletePerson(Long id){
+        personRepository.deleteById(id);
     }
 }
